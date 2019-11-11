f0ac6beff3dcc0df560c170c53436d1c92b9b0ab
gocris <55960124+gocris@users.noreply.github.com>
2019-11-11 11:52:04 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -19,13 +19,18 @@
 */
 
 /**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+ * End Global Variables 
+ Start Helper Functions */
 
-
-
+let eraseActive = function(target) {
+  	var current = document.getElementsByClassName("active");
+    // erase the current higlighted tag with active
+	if (current.length > 0) {
+    	current[0].className = current[0].className.replace(" active", "");
+    }
+	// Add the active class to the current/clicked item
+    target.className += " active";     
+}
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -39,45 +44,13 @@ let ele = function (query, element, repeat, attribute, attName) {
     for (let i = 0; i < repeat; i++) {
         let att = document.createElement(element);
         att.setAttribute(attribute, attName);
-        //att.textContent = `Section ${i}`;
         target.appendChild(att);
     }
   
     console.log(`${repeat} times the element: ${element} was created in ${query}`);
 }
 
-/*
-let secondEle = function() {
-  	for(let i = 0; i < 4; i++) {
-  		const target = document.getElementsByClassName('navItem')[i];
-        let idTag = `#section${i}`;
-      
-    	for (let j = 0; j < 1; j++) {
-          let att = document.createElement('a');
-          att.setAttribute('href', idTag);
-          att.textContent = `Section ${i}`;
-          target.appendChild(att);
-      }
-  
-    }
-}
-*/
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-ele('#navbar__list', 'li', 4, 'class', 'navItem');
-//secondEle();
-
-(function(title, content1, content2, content3) {
+ let navItemTextCont = function(title, content1, content2, content3) {
   
   const heading = document.querySelector('h1');
   heading.textContent = title;
@@ -88,60 +61,61 @@ ele('#navbar__list', 'li', 4, 'class', 'navItem');
   item[1].textContent = content1;
   item[2].textContent = content2;
   item[3].textContent = content3;
+} 
 
-})('Landing Page', 'Section 1', 'Section 2', 'Section 3'); 
 
+ele('#navbar__list', 'li', 4, 'class', 'navItem');
+
+navItemTextCont('Landing Page', 'Section 1', 'Section 2', 'Section 3');
+
+//Begin Events
 
 (function() { 
   const target = document.getElementsByClassName('navItem');
   
-    target[0].addEventListener('click', function() { 
+  target[0].addEventListener('click', function() {
     location.href = '#top';
-  });  
-    
+    eraseActive(target[0]);
+  });
+  
   target[1].addEventListener('click', function() { 
     location.href = '#section1';
+    eraseActive(target[1]);
   });
   
   target[2].addEventListener('click', function() { 
     location.href = '#section2';
+    eraseActive(target[2]);
   });
   
   target[3].addEventListener('click', function() { 
     location.href = '#section3';
+    eraseActive(target[3]);
   });
   
-  
 })();
-
 
 (function() { 
     const target = document.querySelectorAll('.navItem');
     
     for (let i = 0; i < target.length; i++) {
         target[i].addEventListener('mouseenter', function() { 
-            target[i].style.cssText = 'color: white; background: #19DF6F; cursor: pointer; transition: 1s';
+            target[i].style.cssText = 'color: white; background: #19DF6F; cursor: pointer; transition: .5s';
         });
-        
+      
+       document.getElementsByClassName('box')[i].addEventListener('mouseover', function() { 
+           	var current = document.getElementsByClassName("active");
+         // If there's no active class
+    		if (current.length > 0) {
+     		current[0].className = current[0].className.replace(" active", "");
+            }
+
+            // Add the active class to the current/clicked button
+            target[i].className += " active";          
+        });                          
+   
          target[i].addEventListener('mouseleave', function() { 
             target[i].style.cssText = 'transition: 1s; color: black;';
         });
     }
 })();
-
-
-
-
-
-/*
-(function() { 
-  const target = document.getElementsByClass('navItem')[0];
-  
-})();
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
